b322d49334713c85ea20cf917f6667c23f1c6f1c
unknown <skoripalli@gmail.com>
2019-01-16 00:38:26 +0000

    first commit

---
+++ src/PracticeCondoperation/TryClass.java
@@ -0,0 +1,9 @@
+package PracticeCondoperation;
+
+public class TryClass {
+    public static void main(String[] args) {
+        int a=100; int b=200;
+        String result1 = (a>=b) ? "apple" : " bananna" ;
+        System.out.println(result1);
+    }
+}
